5a4d98b291ff6865e6fc3b1fd70a6feee42af302
TJmkc <tangj2@rpi.edu>
2020-12-15 11:01:43 -0500

    Update SVM in Class.R

--- SVM in Class.R
+++ SVM in Class.R
@@ -6,8 +6,11 @@ library(e1071)
 
 qplot(Petal.Length,Petal.Width,data = iris, color = Species)
 help("svm")
+iris
 svm_model1 <- svm(Species~., data = iris)
 summary(svm_model1)
+iris
+
 plot(svm_model1, data = iris, Petal.Width~Petal.Length, slice = list(Sepal.Width = 3, Sepal.Length = 4))
 pred1 <- predict(svm_model1, iris)
 table1 <- table(Predicted = pred1, Actual = iris$Species)
@@ -38,3 +41,5 @@ plot(svm_model3, data = iris, Petal.Width~Petal.Length, slice = list(Sepal.Width
 pred3 <- predict(svm_model3, iris)
 table3 <- table(Predicted = pred3, Actual = iris$Species)
 table3
+
+iris$Species
